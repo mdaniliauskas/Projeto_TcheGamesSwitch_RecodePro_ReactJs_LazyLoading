013e831b4933a7447a3386ef3c62e1e85e1a1ffb
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2021-02-14 14:08:20 -0300

    Aplicando o Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
-import Menu from './Components/Menu';
-import Rodape from './Components/Rodape'
-import Rotas from './rotas';
 import { BrowserRouter } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import { lazy, Suspense } from 'react';
+
+const Menu = lazy (() => import('./Components/Menu'));
+const Rotas = lazy(() => import('./rotas'))
+const Rodape = lazy(() => import('./Components/Rodape'))
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <header>
-        <Menu />
+        <Suspense fallback={ <p>Carregando... Aguarde por favor</p>}>
+          <Menu />
+        </Suspense>        
       </header>
+      
       <main>
         <Container>
+        <Suspense fallback={ <p>Carregando... Aguarde por favor</p>}>
           <Rotas />
+        </Suspense>   
         </Container>
       </main>
       <br /><br /><br />
+      
       <footer>
+       <Suspense fallback={ <p>Carregando... Aguarde por favor</p>}>
         <Rodape />
+        </Suspense>   
       </footer>      
     </div>
     </BrowserRouter>    
